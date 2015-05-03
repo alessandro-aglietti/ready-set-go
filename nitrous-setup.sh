38ba0eba3585223b38bc3758a224571a898d70ff
Alessandro Aglietti <alessandro-aglietti@users.noreply.github.com>
2015-05-03 14:25:20 +0200

    Update nitrous-setup.sh

--- nitrous-setup.sh
+++ nitrous-setup.sh
@@ -4,9 +4,9 @@ unzip go_appengine_sdk_linux_amd64-1.9.19.zip
 rm go_appengine_sdk_linux_amd64-1.9.19.zip
 rm /home/action/.parts/bin/*.py
 export PATH=$PATH:/home/action/go_appengine/
-cd /home/workspace/go/
+cd /home/action/workspace/go/
 git clone https://github.com/alessandro-aglietti/ready-set-go
-chmod +x /home/workspace/go/ready-set-go/goapp-serve-nitrous
+chmod +x /home/action/workspace/go/ready-set-go/goapp-serve-nitrous
 cd /home/action/.parts/bin
-ln -s /home/workspace/go/ready-set-go/goapp-serve-nitrous
-cd /home/workspace/go/ready-set-go
+ln -s /home/action/workspace/go/ready-set-go/goapp-serve-nitrous
+cd /home/action//workspace/go/ready-set-go
