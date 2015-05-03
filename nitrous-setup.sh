db4bf135006bbe00ed887fb938a6a70fa661e9e9
Alessandro Aglietti <alessandro-aglietti@users.noreply.github.com>
2015-05-03 14:14:14 +0200

    Create nitrous-setup.sh

---
+++ nitrous-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.19.zip
+unzip go_appengine_sdk_linux_amd64-1.9.19.zip
+rm go_appengine_sdk_linux_amd64-1.9.19.zip
+rm /home/action/.parts/bin/*.py
+export PATH=$PATH:/home/action/go_appengine/
+cd /home/workspace/go/
+git clone https://github.com/alessandro-aglietti/ready-set-go
