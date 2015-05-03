54997eabd33908315de643cc055e6dfc94be4bb4
alessandro-aglietti <alessandro.aglietti@gmail.com>
2015-05-03 13:58:14 +0000

    demo su template base html con commenti

--- hello.go
+++ hello.go
@@ -4,6 +4,8 @@ import (
     "fmt"
     "net/http"
     "html/template"
+    "math/rand"
+	  "strconv"
 )
 
 func init() {
@@ -15,16 +17,28 @@ func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "Hello, world!")
 }
 
-type Model struct {
+type BasicModel struct {
 	Titolo1, Titolo2 string
 }
 func handlerHtmlTemplate(w http.ResponseWriter, r *http.Request) {
-    tmpl, err := template.ParseFiles("templates/basic.html")
-    
-    err = tmpl.Execute(w, Model{"Titolo 1", "Titolo 2"})
   
+    // recupero il template
+    tmpl, err := template.ParseFiles("templates/basic.html")
     if err != nil {
+      // se avvengono errori nel recupero del template mi fermo
       http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+    
+    // creo la struttura dati composta da un titolo fisso ed un valore casuale
+    valoreCasuale := rand.Intn(3000);
+    data := BasicModel{"Sotto un valore casuale da 0 a 3000", strconv.Itoa(valoreCasuale)}
+  
+    // unisco struttura dati e template html
+    err = tmpl.Execute(w, data)
+    if err != nil {
+      // se avvengono errori nella creazione della pagina dinamica mi fermo
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
     }
 }
-
