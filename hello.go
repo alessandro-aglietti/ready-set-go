bed681580a360b711affaf714fd2c36d95b89e37
alessandro-aglietti <alessandro.aglietti@gmail.com>
2015-05-03 13:43:36 +0000

    aggiunto esempio base con template html

--- hello.go
+++ hello.go
@@ -3,12 +3,28 @@ package hello
 import (
     "fmt"
     "net/http"
+    "html/template"
 )
 
 func init() {
     http.HandleFunc("/", handler)
+    http.HandleFunc("/html-template", handlerHtmlTemplate)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "Hello, world!")
 }
+
+type Model struct {
+	Titolo1, Titolo2 string
+}
+func handlerHtmlTemplate(w http.ResponseWriter, r *http.Request) {
+    tmpl, err := template.ParseFiles("templates/basic.html")
+    
+    err = tmpl.Execute(w, Model{"Titolo 1", "Titolo 2"})
+  
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
